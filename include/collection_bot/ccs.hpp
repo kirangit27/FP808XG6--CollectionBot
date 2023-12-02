199d19a46aeb9ea0755feb8097ca5444bc054f76
Kiran S Patil <kiran.suvas.patil@gmail.com>
2023-12-01 19:36:46 -0500

    1.5.2 - added subscriber and callback functions to keep track of competition state

--- include/collection_bot/ccs.hpp
+++ include/collection_bot/ccs.hpp
@@ -23,12 +23,35 @@ class CompetitionARIAC : public rclcpp::Node
 {
     public:
 
-    CompetitionARIAC() : Node("competition_subscriber"){};
+    CompetitionARIAC() : Node("competition_subscriber"){
+        m_callback_group_1 = this->create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive);
+        m_callback_group_2 = this->create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive);
+
+        //subscription callback groups
+        auto subscription_option1 = rclcpp::SubscriptionOptions();
+        subscription_option1.callback_group = m_callback_group_1;
+        auto subscription_option2 = rclcpp::SubscriptionOptions();
+        subscription_option2.callback_group = m_callback_group_2;
+
+        // Subscriber objects            
+        comp_state_sub = this->create_subscription<ariac_msgs::msg::CompetitionState>("/ariac/competition_state", 10, 
+                                                                    std::bind(&CompetitionARIAC::CompetitionStateCallback, this, std::placeholders::_1),subscription_option1);
+
+    };
 
     private:
+
+        bool comp_start{false};
+        bool order_submitted{false};
+         
+        rclcpp::CallbackGroup::SharedPtr m_callback_group_1;
+        rclcpp::CallbackGroup::SharedPtr m_callback_group_2;
+
+        rclcpp::Subscription<ariac_msgs::msg::CompetitionState>::SharedPtr comp_state_sub;
+
+        void CompetitionStateCallback(const ariac_msgs::msg::CompetitionState::SharedPtr msg);
         
         void callServiceStart();
-
         void callServiceEnd();
 
 
--- src/ccs.cpp
+++ src/ccs.cpp
@@ -2,6 +2,40 @@
 #include "../include/ariac_collection_bot/ccs.hpp"
 
 
+void CompetitionARIAC::CompetitionStateCallback(const ariac_msgs::msg::CompetitionState::SharedPtr msg)
+{
+    if (msg->competition_state == ariac_msgs::msg::CompetitionState::READY)
+    {
+        RCLCPP_INFO_STREAM(rclcpp::get_logger("CompState Subscriber"), "Competition state is 1, calling service client to Start Competition...");
+        callService_start();
+    }
+    else if (msg->competition_state == ariac_msgs::msg::CompetitionState::STARTED &&  comp_start == false)
+    {
+        RCLCPP_INFO(rclcpp::get_logger("CompState Subscriber"), "Competition state is 2, Competition Started...\n");
+        comp_start = true;
+    }
+    else if (msg->competition_state == ariac_msgs::msg::CompetitionState::ORDER_ANNOUNCEMENTS_DONE && order_submitted == false)
+    {
+          
+        RCLCPP_INFO(rclcpp::get_logger("CompState Subscriber"), "Competition state is 3, all orders retrived...\n");
+        RCLCPP_INFO_STREAM(rclcpp::get_logger("Order Submission    "), "Submitting Orders ...");
+        order_submitted = true;  
+          
+    }
+    else if (msg->competition_state == ariac_msgs::msg::CompetitionState::ORDER_ANNOUNCEMENTS_DONE && order_submitted == true)
+    {
+        RCLCPP_INFO(rclcpp::get_logger("CompState Subscriber"), "Competition state is 3, all orders order_submitted, calling service client to End Competition...");
+        callService_end();
+        comp_state_sub.reset();
+    }
+    else if (msg->competition_state == ariac_msgs::msg::CompetitionState::ENDED)
+    {
+        RCLCPP_INFO(rclcpp::get_logger("CompState Subscriber"), "Competition state is 4, Competition Ended...");
+    }
+}
+
+
+
 void CompetitionARIAC::callServiceStart()
 {
     auto client = create_client<std_srvs::srv::Trigger>("/ariac/start_competition");
